cb633ade31d994744a30d5e2f4ea27e5c69e73b3
Germione <germione.grangere@gmail.com>
2019-05-17 16:10:20 +0200

    feat: add spider for seloger

---
+++ src/roof_bot/spiders/seloger.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+import random
+from time import sleep
+from math import ceil
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from lxml import html
+from parse import parse
+
+from spiders.base import Spiderman
+
+
+class SeLogerSpider(Spiderman):
+
+    PROVIDER_NAME = "seloger"
+    CSS_SELECTOR_FOR_ADS_NUMBER = 'div.title_nbresult'
+    CSS_SELECTOR_FOR_RESULTS_LIST = 'section.liste_resultat'
+    CSS_SELECTOR_LIST_FOR_ADS = [
+        'div.c-pa-list.c-pa-sl.cartouche',
+        'div.c-pa-list.c-pa-sl9.cartouche',
+        'div.c-pa-list.c-pa-bd.cartouche',
+    ]
+
+    def crawl(self):
+        urls = self.start_urls()
+        ads = self.process(urls)
+        return ads
+
+    def start_urls(self):
+
+        def make_url(base, var, page_number):
+            return base + var + str(page_number)
+
+        listing_var = "&LISTING-LISTpg="
+        nb_ads, nb_pages = self.ads_number()
+        nb_pages = int(nb_pages)
+        for n in range(1, nb_pages + 1):
+            yield make_url(self._search_url, listing_var, n)
+
+    def ads_number(self):
+        content = self.make_request(self._search_url)
+
+        root = html.fromstring(content)
+        # Parse HTML and get total announces string
+        results = root.cssselect(self.CSS_SELECTOR_FOR_ADS_NUMBER)
+        results = results[0].text_content().strip()
+        # Parse String and total number
+        nb_results = parse("{} annonces", results)[0]
+        nb_pages = ceil(int(nb_results) / 20)
+        return (int(nb_results), nb_pages)
+
+    def process_page(self, content):
+        root = html.fromstring(content)
+
+        # Parse HTML and get results list
+        results = root.cssselect(self.CSS_SELECTOR_FOR_RESULTS_LIST)
+        # results is a list of one element so return the first one
+        results =  results[0]
+
+        ads = []
+        for selector in self.CSS_SELECTOR_LIST_FOR_ADS:
+            results = results.cssselect(selector)
+            all_ads.extend(results)
+        return ads
+
+    def process_element(self, element):
+        ad_url = element[1][0].get('href')
+        size = element[1][1][2].text_content()
+        price = element[1][2][1].text_content().strip()
+        city = element[1][4].text_content().strip()
+        ad_id = element[1][5].get('data-idannonce')
+        ad_publication_id = element[1][5].get('data-idpublication')
+        return {
+            'id': ad_id,
+            'pub_id': ad_publication_id,
+            'url': ad_url,
+            'city': city,
+            'size': size,
+            'price': price
+        }
